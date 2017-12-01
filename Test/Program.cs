069b0bf38f28346a9fdf1a7be007b3aa6eaeff6d
Nguyen Hoang Chuong <chuongnh.hcm@gmai.com>
2017-12-01 20:18:03 +0700

    first commit

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace Test
 {
+    public class MODE
+    {
+        public int SignStart { get; set; }
+        public int SignLength { get; set; }
+
+        public int WeightStart { get; set; }
+        public int WeightLength { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            // khởi tạo kiểu loại cân
+            MODE mode = new MODE
+            {
+                SignStart = 7,
+                SignLength = 1,
+                WeightStart = 0,
+                WeightLength = 6
+            };
+            // test case
+            Console.WriteLine(TestFunc(".567890", mode));
+            Console.WriteLine(TestFunc("5.67890-", mode));
+            Console.WriteLine(TestFunc("5.567800", mode));
+            Console.WriteLine(TestFunc("5.56780", mode));
+            Console.WriteLine(TestFunc("5.56780-", mode));
+        }
+
+        // hàm chính
+        static double TestFunc(string test, MODE mode)
+        {
+            string digit = Reverse(test.Substring(mode.WeightStart, mode.WeightLength).ToString());
+
+            string sign = "";
+            if (test.Length > 7)
+            {
+                sign = test.Substring(mode.SignStart, mode.SignLength);
+            }
+
+            return Double.Parse(sign + digit);
+        }
+
+        // đảo chuổi
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
